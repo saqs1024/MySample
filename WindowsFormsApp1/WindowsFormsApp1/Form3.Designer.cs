7bebed7db224bdae6c12802c4bf68a080e63af18
saqs1024 <Saqs1024@gmail.com>
2020-04-05 01:06:16 -0400

    RAJU Added new adress and residential bars in forms

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form3.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SUBMIT = new System.Windows.Forms.Button();
+            this.Adress = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // SUBMIT
+            // 
+            this.SUBMIT.Location = new System.Drawing.Point(365, 312);
+            this.SUBMIT.Name = "SUBMIT";
+            this.SUBMIT.Size = new System.Drawing.Size(75, 23);
+            this.SUBMIT.TabIndex = 0;
+            this.SUBMIT.Text = "SUBMIT";
+            this.SUBMIT.UseVisualStyleBackColor = true;
+            // 
+            // Adress
+            // 
+            this.Adress.AutoSize = true;
+            this.Adress.Location = new System.Drawing.Point(211, 77);
+            this.Adress.Name = "Adress";
+            this.Adress.Size = new System.Drawing.Size(39, 13);
+            this.Adress.TabIndex = 1;
+            this.Adress.Text = "Adress";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(365, 77);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(203, 51);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(211, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Residential Adress";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(365, 184);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(203, 51);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Adress);
+            this.Controls.Add(this.SUBMIT);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SUBMIT;
+        private System.Windows.Forms.Label Adress;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
